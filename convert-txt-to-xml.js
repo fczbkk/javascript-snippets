10f06d5ad9e7efe58fea87383075de1e414dbd91
Riki Fridrich <riki@fczbkk.com>
2012-07-26 14:02:01 +0200

    convertTxtToXml

---
+++ convert-txt-to-xml.js
@@ -0,0 +1,20 @@
+/*
+Simple way of converting text to XML document. This comes handy when you
+receive XML data via Ajax.
+*/
+
+function convertTxtToXml(text, callback) {
+    text = text || '';
+    callback = callback || function () {};
+
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(options.text, "application/xml");
+
+    callback(doc);
+    return doc;    
+}
+
+// A simple example:
+var txt = '<items><item value="1" /><item value="2" /><item value="3" /></items>';
+var data = convertTxtToXml(txt);
+var items = data.getElementsByTagName('item');
