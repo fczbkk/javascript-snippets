14a02ec77eb24e230999a96470868fbc9ad0662b
Riki Fridrich <riki@fczbkk.com>
2013-05-10 21:42:50 +0200

    documentation

--- compare-versions/compare-versions.js
+++ compare-versions/compare-versions.js
@@ -1,3 +1,11 @@
+/*
+Function to compare two version strings (e.g. '1.0.0' or '26.0.1410.64').
+Expects two arguments. Returns 'null' object if any of the arguments is wrong.
+Returns 0 if versions are the same.
+Returns -1 if left version (first argument) is lower.
+Returns 1 if left version (first argument) is higher.
+*/
+
 function compareVersions(left, right) {
     var result = null;
 
